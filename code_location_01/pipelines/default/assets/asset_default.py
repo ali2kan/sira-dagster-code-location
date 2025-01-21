503c54f90ddcf6b0e67b7970aa9ab58ac8a9890a
ali2kan <60204416+ali2kan@users.noreply.github.com>
2025-01-21 15:22:25 -0800

    🩸First Blood

--- code_location_01/pipelines/default/assets/asset_default.py
+++
@@ -1,6 +0,0 @@
-from dagster import asset
-
-@asset
-def asset_default():
-    """A simple default asset."""
-    return "Hello from default asset!"
--- code_location_01/pipelines/default/job/etl_pipeline.py
+++
@@ -1,6 +0,0 @@
-from dagster import graph
-
-@graph
-def etl_op_graph():
-    """A simple ETL graph."""
-    pass
